5e5b533f1edf6f4588625b5db92e70202e080f5b
FavaSX <favaowo@gmail.com>
2024-11-24 23:30:55 -0300

    Keystore add

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cl.duoc.asistenciaduoc',
   appName: 'registro-app',
   webDir: 'www',
   plugins: {
